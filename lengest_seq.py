67b4fa7a36d0bf9971c1a7cab1da37feda0854f3
Dhanush Hegde <dhanushhegde15@gmail.com>
2024-09-19 23:17:37 +0530

    y1

--- lengest_seq.py
+++ lengest_seq.py
@@ -6,6 +6,9 @@ class Solution:
         longest = 0
 
         for n in numSet:
+
+
+            
             # check if its the start of a sequence
             if (n - 1) not in numSet:
                 length = 1
